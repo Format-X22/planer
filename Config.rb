ee0278295610127cd51f657fc104aa8bdbd00ea9
Oleg <oleg.pav.m@gmail.com>
2018-04-03 15:42:53 +0300

    Config.

---
+++ Config.rb
@@ -0,0 +1,56 @@
+require 'json'
+
+class Config
+
+	def initialize
+		@rescue_read = 0
+	end
+
+	def open?
+		config[:open]
+	end
+
+	def close?
+		config[:close]
+	end
+
+	def long?
+		config[:long]
+	end
+
+	def short?
+		config[:short]
+	end
+
+	def stop?
+		config[:stop]
+	end
+
+	private
+
+	def config
+		data = JSON.restore File.read 'config.json'
+
+		data.each do |key, value|
+			case value
+				when 'yes' then data[key.to_sym] = true
+				when 'no'  then data[key.to_sym] = false
+				else raise ''
+			end
+		end
+
+		@rescue_read = 0
+
+		data
+	rescue
+		@rescue_read += 1
+
+		if @rescue_read > 10
+			raise 'Bad config'
+		end
+
+		sleep 1
+		config
+	end
+
+end
